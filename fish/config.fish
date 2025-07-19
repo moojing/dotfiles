a650a00274dd35efb500bdb7d37db956afba58fa
moojing <starts789@gmail.com>
2025-07-19 14:59:38 +0100

    feat: fix the shell syntax in config file.

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,19 @@
-export PATH="/usr/local/bin:$PATH"
-export PATH="$HOME/.cargo/bin:$PATH"
-export PATH="/Users/mujingtsai/solana-release/bin:$PATH"
-export PATH="/Users/mujingtsai/Library/Python/3.7/bin:$PATH"
-export PATH="$HOME/.bin:$PATH"
+set -gx PATH /opt/homebrew/bin $PATH
+# export PATH="/usr/local/bin:$PATH"
+set -gx PATH $HOME/.cargo/bin $PATH
+set -gx PATH /Users/mujingtsai/solana-release/bin $PATH
+set -gx PATH /Users/mujingtsai/Library/Python/3.7/bin $PATH
 
-set PATH $PATH "$HOME/.bin"
+# Add node to 
+fnm env --shell=fish | source
+
+set -gx PATH $HOME/.rbenv/shims $PATH
+
 
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/mujingtsai/google-cloud-sdk/path.fish.inc' ]; . '/Users/mujingtsai/google-cloud-sdk/path.fish.inc'; end
+if test -f $HOME/google-cloud-sdk/path.fish.inc
+    source $HOME/google-cloud-sdk/path.fish.inc
+end
 
-# solana cli
-export PATH="/Users/mujingtsai/.local/share/solana/install/active_release/bin:$PATH"
-export PATH="$PATH:/Users/mujingtsai/.foundry/bin"
+oh-my-posh --init --shell fish --config (brew --prefix oh-my-posh)/themes/json.omp.json | source
 
-oh-my-posh --init --shell fish --config (brew --prefix oh-my-posh)"/themes/json.omp.json" | source
